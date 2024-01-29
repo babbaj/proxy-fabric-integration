a67dca0972f00ea751fdcf6d909192a6ffa587ce
Babbaj <babbaj45@gmail.com>
2024-01-29 00:47:24 -0500

    fix overlap with multiple checkboxes

--- src/main/java/dev/babbaj/proxyintegration/CheckboxRow.java
+++ src/main/java/dev/babbaj/proxyintegration/CheckboxRow.java
@@ -1,28 +1,6 @@
 package dev.babbaj.proxyintegration;
 
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.multiplayer.MultiplayerServerListWidget;
 import net.minecraft.client.gui.widget.CheckboxWidget;
 import net.minecraft.client.network.ServerInfo;
-import net.minecraft.text.Text;
 
-public class CheckboxRow extends MultiplayerServerListWidget.Entry {
-    public final ServerInfo parent;
-
-    public final CheckboxWidget[] checkboxes;
-
-    public CheckboxRow(ServerInfo parent, CheckboxWidget[] checkboxes) {
-        this.parent = parent;
-        this.checkboxes = checkboxes;
-    }
-
-    @Override
-    public Text getNarration() {
-        return Text.empty();
-    }
-
-    @Override
-    public void render(DrawContext context, int index, int y, int x, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean hovered, float tickDelta) {
-
-    }
-}
+public record CheckboxRow(ServerInfo parent, CheckboxWidget[] checkboxes) { }
